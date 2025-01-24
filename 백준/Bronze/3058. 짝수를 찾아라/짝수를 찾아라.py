ae88237e2a2ff4522c3948525a74f21cd527fcf1
Chaeeun Bae <106861866+chaeengg@users.noreply.github.com>
2025-01-24 22:07:15 +0900

    [Bronze III] Title: 짝수를 찾아라, Time: 92 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/3058. 짝수를 찾아라/짝수를 찾아라.py
+++ 백준/Bronze/3058. 짝수를 찾아라/짝수를 찾아라.py
@@ -1,18 +1,13 @@
 T = int(input())
-step = 0
 
-while True:
-    step += 1
-    sum = 0 
-    min_list = []       
-    num_list = []
-    num_list[0:7] = map(int, input().split())
+for i in range(T):
+    sum = 0
+    min_list = []
+    num_list= list(map(int, input().split()))
 
-    for i in range(7):
-        if num_list[i] % 2 == 0:
-            sum += num_list[i]
-            min_list.append(num_list[i])
+    for j in range(7):
+        if num_list[j] % 2 == 0:
+            sum += num_list[j]
+            min_list.append(num_list[j])
+
     print(sum, min(min_list))
-
-    if step == T:
-        break
