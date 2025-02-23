a3d67723e43044dfa4adb6057f3c856ed7217bc7
Chaeeun Bae <106861866+chaeengg@users.noreply.github.com>
2025-02-23 17:28:59 +0900

    [Bronze V] Title: 자동완성, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/24883. 자동완성/자동완성.py
@@ -0,0 +1,9 @@
+while True:
+    try:
+        sen = input()
+        if sen == 'N' or sen == 'n':
+            print('Naver D2')
+        else:
+            print('Naver Whale')
+    except:
+        break
